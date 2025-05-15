e4cd7c29f8945e08e95b5f9cee200eece120c755
ARTHUR SANTOS TAVARES SOUTO <14255515999@senacrs.edu.br>
2025-05-15 17:11:13 -0300

    add function calculator

--- src/others.js
+++ src/others.js
@@ -1,7 +1,26 @@
 
 // teste
 
-function somar(num1, num2){
-    console.log(num1 + num2)
+function calculator(target, n1, n2){
+    let calc = 0;
+    switch(target) {
+        case  "+":  
+            calc = n1 + n2
+        break
+        case  '-':
+            calc = n1 - n2
+        break
+        case  '/':
+            calc = n1 / n2;
+            break
+        case  '*':
+            calc = n1 * n2
+        break
+    }
+
+    return calc
 }
 
+
+
+console.log(calculator("*", 5, 5))
